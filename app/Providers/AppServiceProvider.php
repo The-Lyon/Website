10fab38b9241f789af55ea425b34fb8e4ab35151
Liam <mailtelenko@gmail.com>
2018-04-01 20:10:05 -0400

    HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,11 +11,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if(env('REDIRECT_HTTPS'))
-        {
-            $url->forceSchema('https');
+        //HTTPS
+        if(env('APP_ENV') == 'production') {
+            \URL::forceSchema('https');
         }
     }
 
